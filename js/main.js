f7fd5d099e1acb177077e4bd4454f7e892da39c8
Mohamed Ammar <ammarmohamed074@gmail.com>
2020-12-01 20:22:37 +0200

    Adding Project Files

---
+++ js/main.js
@@ -0,0 +1,309 @@
+$(window).on('load', function() {
+
+    $(".loading-overlay .loading-overlay-box").fadeOut(500,
+        function() {
+            $(this).parent().fadeOut(500);
+            $("body").removeClass("overflow")
+        });
+})
+
+
+// Scroll To Top Button 
+var scrollButton = $(".scroll-top");
+$(window).scroll(function() {
+    ($(this).scrollTop() >= 500) ? scrollButton.show(600): scrollButton.hide(600);
+});
+// Click Button to scroll top 
+scrollButton.click(function() {
+    $("html,body").animate({ scrollTop: 0 }, 600);
+});
+
+
+
+var wow = new WOW({
+    boxClass: 'wow', // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: true, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    callback: function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null, // optional scroll container selector, otherwise use window,
+    resetAnimation: true, // reset animation on end (default is true)
+});
+wow.init();
+
+
+
+$(document).ready(function() {
+
+    if ($(window).width() >= 992) {
+        // This is To Open search Box
+        $(".cats .nav-link").click(function() {
+            $(".overlay-box").fadeIn(300);
+        });
+
+        $(".cats .nav-link").click(function() {
+            $(".nav-link .cats-name").addClass("rotate");
+            $(".nav-link .cats-name").removeClass("rotate-reverse");
+            $(".cats .cats-list").slideDown(300);
+        });
+
+        $(".overlay-box").click(function() {
+            $(".overlay-box").fadeOut(300);
+        });
+
+        $(".overlay-box").click(function() {
+            $(".nav-link .cats-name").removeClass("rotate");
+            $(".nav-link .cats-name").addClass("rotate-reverse");
+            $(".cats .cats-list").slideUp(300);
+
+        });
+    }
+    if ($(window).width() <= 991) {
+        $(".cats .nav-link").click(function() {
+            $(".nav-link .cats-name").toggleClass("rotate");
+            $(".cats .cats-list").slideToggle(300);
+        });
+    }
+    //Main Slider Carousel
+    $('.main-slider .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        rtl: true,
+        margin: 20,
+        navText: ["<span class='fas fa-chevron-right'></span>", "<span class='fas fa-chevron-left'></span>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                dots: true,
+                loop: true
+            },
+            768: {
+                items: 1,
+                nav: false,
+                dots: true,
+                loop: true
+            },
+            992: {
+                items: 1,
+                nav: false,
+                dots: true,
+                loop: true
+            },
+            1200: {
+                items: 1,
+                nav: true,
+                dots: true,
+                loop: true
+            }
+        }
+    });
+
+
+    $('.bestsellers .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        margin: 20,
+        stagePadding: 5,
+        rtl: true,
+        navText: ["<span class='fa fa-chevron-right'></span>", "<span class='fa fa-chevron-left'></span>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+                dots: true,
+                nav: false,
+                loop: true,
+                stagePadding: 15,
+            },
+            500: {
+                items: 2,
+                dots: true,
+                nav: false,
+                loop: true,
+            },
+            720: {
+                items: 3,
+                dots: true,
+                nav: false,
+                loop: true
+            },
+            992: {
+                items: 4,
+                nav: false,
+                dots: true,
+                loop: true
+            },
+            1200: {
+                items: 4,
+                nav: false,
+                dots: true,
+                loop: true
+            }
+        }
+    });
+    $(".mo-tabs1 a").on("shown.bs.tab", function(e) {
+        let $owl = $(".bestsellers .owl-carousel");
+        $owl.trigger('refresh.owl.carousel');
+    });
+
+    //This is To make faetures as slider in small screens
+    if ($(window).width() <= 991) {
+        $(".features .features-box").addClass("owl-carousel owl-theme");
+        $(".features-box .feature").removeClass("owl-carousel");
+        $('.features .owl-carousel').owlCarousel({
+            loop: false,
+            margin: 20,
+            rtl: true,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false,
+                    loop: false
+                },
+                500: {
+                    items: 2,
+                    nav: false,
+                    loop: false
+                }
+            }
+        })
+    } else {
+        $(".features-box .feature").removeClass("owl-carousel");
+        $(".features-box .feature").removeClass("owl-carousel");
+    };
+
+    // Footer Section Start --------------------------------------------------------------------------------------------------------------------
+    // To Toggle Between Footer Slide Menus
+    if ($(window).width() <= 991) {
+        $('.collapse[data-parent="#parent"]').on('show.bs.collapse', function() {
+            $('.collapse[data-parent="#parent"]').not(this).collapse('hide');
+        });
+    } else {
+        $(".slide-list").addClass("in")
+    }
+    //This is To Change between footer collapse link ----------------------------------------------------------------
+    // First Collapse ---------------------------
+    $(".first .footer-heading .footer-link").click(function() {
+        var x = $(".first .slide-list")
+        if (x.hasClass("collapse")) {
+            $(".first .footer-heading .footer-link").addClass("rotate")
+            $(".first .footer-heading .footer-link").removeClass("rotate-reverse")
+            $(".second .footer-heading .footer-link").removeClass("rotate")
+            $(".second .footer-heading .footer-link").addClass("rotate-reverse")
+            $(".last .footer-heading .footer-link").removeClass("rotate")
+            $(".last .footer-heading .footer-link").addClass("rotate-reverse")
+        }
+    });
+    $(".first .footer-heading .footer-link").click(function() {
+        var x = $(".first .slide-list")
+        if (x.hasClass("in")) {
+            $(".first .footer-heading .footer-link").removeClass("rotate")
+            $(".first .footer-heading .footer-link").addClass("rotate-reverse")
+        }
+    });
+
+    // Second Collapse ---------------------------
+    $(".second .footer-heading .footer-link").click(function() {
+        var x = $(".second .slide-list")
+        if (x.hasClass("collapse")) {
+            $(".second .footer-heading .footer-link").addClass("rotate")
+            $(".second .footer-heading .footer-link").removeClass("rotate-reverse")
+            $(".first .footer-heading .footer-link").removeClass("rotate")
+            $(".first .footer-heading .footer-link").addClass("rotate-reverse")
+            $(".last .footer-heading .footer-link").removeClass("rotate")
+            $(".last .footer-heading .footer-link").addClass("rotate-reverse")
+        }
+    });
+    $(".second .footer-heading .footer-link").click(function() {
+        var x = $(".second .slide-list")
+        if (x.hasClass("in")) {
+            $(".second .footer-heading .footer-link").removeClass("rotate")
+            $(".second .footer-heading .footer-link").addClass("rotate-reverse")
+        }
+    });
+
+    // Last Collapse ---------------------------
+    $(".last .footer-heading .footer-link").click(function() {
+        var x = $(".last .slide-list")
+        if (x.hasClass("collapse")) {
+            $(".last .footer-heading .footer-link").addClass("rotate")
+            $(".last .footer-heading .footer-link").removeClass("rotate-reverse")
+            $(".first .footer-heading .footer-link").removeClass("rotate")
+            $(".first .footer-heading .footer-link").addClass("rotate-reverse")
+            $(".second .footer-heading .footer-link").removeClass("rotate")
+            $(".second .footer-heading .footer-link").addClass("rotate-reverse")
+        }
+    });
+    $(".last .footer-heading .footer-link").click(function() {
+        var x = $(".last .slide-list")
+        if (x.hasClass("in")) {
+            $(".last .footer-heading .footer-link").removeClass("rotate")
+            $(".last .footer-heading .footer-link").addClass("rotate-reverse")
+        }
+    });
+
+    // This is To Open search Box
+    $(".terms .search").click(function() {
+        $("body").addClass("overflow");
+        $(".overlay-box2").slideDown(500);
+    });
+
+    $(".terms .search").click(function() {
+        $(".search .search-icon").removeClass("open-search")
+        $(".search .search-icon").addClass("close-search")
+        $(".close-search-btn").fadeIn(300)
+        $(".search-box").fadeIn(300);
+    });
+
+    $(".close-search-btn,.overlay-box2").click(function() {
+        $("body").removeClass("overflow");
+        $(".overlay-box2").slideUp(500);
+    });
+
+    $(".close-search-btn,.overlay-box2").click(function() {
+        $(".search .search-icon").addClass("open-search")
+        $(".search .search-icon").removeClass("close-search")
+        $(".close-search-btn").fadeOut(300)
+        $(".search-box").fadeOut(300);
+    });
+
+
+    //This is to Open Side Menu in Small Screens
+    $(".menu").click(function() {
+        $("body").addClass("overflow")
+        $(".menu .menu-icon").removeClass("open-menu")
+        $(".menu .menu-icon").addClass("close-menu")
+        $(".mo-nav").addClass("menu-open");
+        $(".overlay-box3").fadeIn(500);
+    });
+
+    $(".close,.overlay-box3").click(function() {
+        $("body").removeClass("overflow")
+        $(".menu .menu-icon").addClass("open-menu")
+        $(".menu .menu-icon").removeClass("close-menu")
+        $(".mo-nav").removeClass("menu-open");
+        $(".overlay-box3").fadeOut(500);
+    });
+
+
+    $(".cats-li .cats-link").click(function() {
+        $(".cats-li .cats-link").toggleClass("rotate");
+        $(".cats-li .sub-list").css("transition", "none");
+        $(".cats-li .sub-list").slideToggle(300);
+    });
+
+    $(".config-box .nav-link").click(function() {
+        $(".config-box .nav-link .config-name").toggleClass("rotate");
+        $(".mo-nav .config-list").css("transition", "none");
+        $(".mo-nav .config-list").slideToggle(300);
+    });
+});
